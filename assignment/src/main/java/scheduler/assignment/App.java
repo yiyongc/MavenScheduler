a2ddda047846b8b0ef1da7188566a73b0e7e89ad
Yi Yong <yi-yong.chee@capgemini.com>
2017-07-20 14:05:53 +0800

    First commit

---
+++ assignment/src/main/java/scheduler/assignment/App.java
@@ -0,0 +1,13 @@
+package scheduler.assignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
